0b68a6b6125ed093ca330f5322c35ac2e2f2b2e8
Beirami <ahoora.beirami@gmail.com>
2026-09-03 12:33:12 +0330

    Add login view

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.views import View
 
 from .forms import UserLoginForm
@@ -7,4 +8,19 @@ from .forms import UserLoginForm
 class UserLogin(View):
     def get(self, request):
         form = UserLoginForm()
-        return render(request,'account/login.html',{'form': form})
+        return render(request, 'account/login.html', {'form': form})
+
+    def post(self, request):
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['phone_number'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                form.add_error('password', "invalid data")
+        else:
+            form.add_error('password', "invalid data")
+
+        return render(request, 'account/login.html', {'form': form})
