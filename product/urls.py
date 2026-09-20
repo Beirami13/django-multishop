7ab76ecfb6581dde2e0b027f7ca8516ff25a1f70
Beirami <ahoora.beirami@gmail.com>
2026-09-20 23:09:38 +0330

    update product filters and categories

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-
-from product.admin import CategoryAdmin
-from product.views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView
 
 app_name = 'product'
+
 urlpatterns = [
-    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('category/<slug:slug>/',ProductListView.as_view(),name='category'),
+    path('', ProductListView.as_view(), name='products'),
+
+    path(
+        'category/<slug:slug>/',
+        ProductListView.as_view(),
+        name='category'
+    ),
+
+    path(
+        '<int:pk>/',
+        ProductDetailView.as_view(),
+        name='detail'
+    ),
 ]
--- product/views.py
+++ product/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import DetailView, TemplateView, ListView
-
+from django.views.generic import DetailView, ListView
+from django.views.generic import ListView
 from product.models import Product, Category
-
 
 class ProductDetailView(DetailView):
     template_name = 'product/product.html'
     model = Product
-
 
 class ProductListView(ListView):
     model = Product
@@ -16,11 +13,26 @@ class ProductListView(ListView):
     paginate_by = 9
 
     def get_queryset(self):
-        queryset = Product.objects.all()
 
+        queryset = Product.objects.all()
         slug = self.kwargs.get('slug')
 
         if slug:
-            queryset = queryset.filter(category__slug=slug)
+            queryset = queryset.filter(
+                category__slug=slug
+            )
 
-        return queryset
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+
+        if min_price:
+            queryset = queryset.filter(
+                price__gte=min_price
+            )
+
+        if max_price:
+            queryset = queryset.filter(
+                price__lte=max_price
+            )
+
+        return queryset.distinct()
